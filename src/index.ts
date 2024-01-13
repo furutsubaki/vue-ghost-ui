413aa535263f3a8cb6a61eed920599dbbdd63148
古椿 <tsubaki@shigurezuki.jp>
2024-01-14 04:55:27 +0900

    index.tsの型修正

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,17 @@ import '@acab/reset.css';
 import '@/assets/css/style.css';
 import '@/assets/css/variables.css';
 
+import type { Component } from 'vue'
 import Button from '@/components/organisms/formParts/Button.vue';
 import Textarea from '@/components/organisms/formParts/Textarea.vue';
 
-const Components = {
+const Components: { [key: string]: Component } = {
     Button,
     Textarea
 };
 
 export default {
-    install(app, options = {}) {
+    install(app: any) {
         Object.keys(Components).forEach((component) => {
             app.component(`Vgu${component}`, Components[component]);
         });
