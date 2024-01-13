546127a6478383211bdd5bd42377b80fa4da2771
古椿 <tsubaki@shigurezuki.jp>
2024-01-14 04:52:06 +0900

    サイト側の調整

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import Button from './components/organisms/formParts/Button.vue';
-import Textarea from './components/organisms/formParts/Textarea.vue';
-
-const model = ref('');
 </script>
 
 <template>
     <div>
-        <Button>ゴーストボタン</Button>
-        <Textarea v-model="model" />{{ model }}
+        <router-view></router-view>
     </div>
 </template>
 
