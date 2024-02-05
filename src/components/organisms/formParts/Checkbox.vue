237b7e98ba8115ac5ba429b642d7dd2ed76c80f7
古椿 <tsubaki@shigurezuki.jp>
2024-02-06 06:12:30 +0900

    チェックボックス系のコンポーネント実装

---
+++ src/components/organisms/formParts/Checkbox.vue
@@ -0,0 +1,341 @@
+<script setup lang="ts" generic="T">
+import { type Ref, computed, watch } from 'vue';
+import { useField } from 'vee-validate';
+import { ZodNumber, ZodString, ZodNullable, ZodBoolean, ZodLiteral } from 'zod';
+
+const model = defineModel<string | number | boolean>();
+const props = withDefaults(
+    defineProps<{
+        /**
+         * 項目値
+         */
+        value?: string | number | boolean;
+        /**
+         * フィールド名
+         */
+        name?: string;
+        /**
+         * zodスキーマ
+         */
+        schema?:
+            | ZodLiteral<T>
+            | ZodBoolean
+            | ZodNullable<ZodBoolean>
+            | ZodString
+            | ZodNullable<ZodString>
+            | ZodNumber
+            | ZodNullable<ZodNumber>;
+        /**
+         * 見出し
+         */
+        label?: string;
+        /**
+         * 無効か
+         */
+        disabled?: boolean;
+        /**
+         * 表示種類
+         */
+        valiant?: 'primary' | 'secondary' | 'info' | 'success' | 'warning' | 'danger';
+        /**
+         * サイズ
+         */
+        size?: 'small' | 'medium' | 'large';
+        /**
+         * エラーメッセージを表示するか
+         */
+        isErrorMessage?: boolean;
+    }>(),
+    {
+        value: true,
+        name: Math.random().toString(),
+        schema: undefined,
+        label: '',
+        disabled: false,
+        valiant: 'secondary',
+        size: 'medium',
+        isErrorMessage: true
+    }
+);
+const unCheckValue = computed(() => (typeof props.value === 'boolean' ? false : ''));
+
+const {
+    value: fieldVal,
+    checked,
+    errors,
+    handleChange
+} = useField<string | number | boolean>(props.name, undefined, {
+    type: 'checkbox',
+    checkedValue: props.value,
+    uncheckedValue: unCheckValue.value
+});
+
+const schemaChunks = computed(() => (props.schema as ZodString)?._def.checks || []);
+const isRequired = computed(
+    () => schemaChunks.value.some((check) => check.kind === 'min' && check.value === 1) || false
+);
+
+const onChange = (event: Event) => {
+    let val = (event.target as HTMLInputElement).value as string | number | boolean;
+
+    if (!(event.target as HTMLInputElement).checked) {
+        val = unCheckValue.value;
+    }
+    handleChange(val);
+};
+
+watch(checked as Ref<boolean>, (flg) => {
+    model.value = flg ? props.value : unCheckValue.value;
+});
+
+if (fieldVal.value == null && model.value != null) {
+    fieldVal.value = model.value;
+}
+</script>
+
+<template>
+    <div class="component-checkbox">
+        <div v-if="label" class="label-placeholder" :class="{ required: isRequired }">
+            {{ label }}
+        </div>
+        <div class="item-label">
+            <label class="input" :class="[valiant, size, { disabled: disabled }]">
+                <input
+                    class="checkbox"
+                    type="checkbox"
+                    :value="value"
+                    :name="name"
+                    :disabled="disabled"
+                    :checked="checked"
+                    @change="onChange"
+                />
+                <slot />
+            </label>
+        </div>
+        <template v-if="isErrorMessage">
+            <div v-for="error in errors" :key="error" class="error">{{ error }}</div>
+        </template>
+    </div>
+</template>
+
+<style scoped>
+.component-checkbox {
+    position: relative;
+    text-align: left;
+    display: block;
+    padding: 8px 0;
+}
+
+.label-placeholder {
+    position: absolute;
+    top: -0.5em;
+    left: 0em;
+    height: 1em;
+    line-height: 1em;
+    pointer-events: none;
+    transition: 0.2s;
+    display: flex;
+    align-items: baseline;
+    font-size: var(--font-size-small);
+    color: var(--color-theme-text-primary);
+    &.required {
+        &::after {
+            left: -0.5em;
+            color: var(--color-status-danger);
+            content: '*';
+        }
+    }
+}
+
+.item-label {
+    position: relative;
+    text-align: left;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    white-space: nowrap;
+
+    .input {
+        display: flex;
+        gap: 16px;
+        align-items: center;
+        justify-content: flex-start;
+        line-height: 1.5em;
+        transition:
+            color 0.2s,
+            background-color 0.2s,
+            border-color 0.2s,
+            opacity 0.2s;
+        &:disabled {
+            pointer-events: none;
+            opacity: 0.5;
+        }
+    }
+}
+
+.primary {
+    color: var(--color-base-white);
+    background-color: var(--color-theme-active);
+    border-color: var(--color-theme-active);
+
+    &:focus {
+        border-color: var(--color-theme-active);
+    }
+
+    @media (hover: hover) {
+        &:hover {
+            color: var(--color-theme-active);
+            background-color: transparent;
+            border-color: var(--color-theme-active);
+        }
+    }
+
+    @media (hover: none) {
+        &:active {
+            color: var(--color-theme-active);
+            background-color: transparent;
+            border-color: var(--color-theme-active);
+        }
+    }
+}
+
+.secondary {
+    color: var(--color-theme-text-primary);
+    background-color: transparent;
+    border-color: var(--color-theme-border);
+
+    &:focus {
+        border-color: var(--color-theme-active);
+    }
+
+    @media (hover: hover) {
+        &:hover {
+            background-color: transparent;
+            border-color: var(--color-theme-active);
+        }
+    }
+
+    @media (hover: none) {
+        &:active {
+            background-color: transparent;
+            border-color: var(--color-theme-active);
+        }
+    }
+}
+
+.info {
+    color: var(--color-theme-text-primary);
+    background-color: var(--color-status-info);
+    border-color: var(--color-status-info);
+
+    &:focus {
+        border-color: var(--color-theme-info);
+    }
+
+    @media (hover: hover) {
+        &:hover {
+            background-color: transparent;
+            border-color: var(--color-status-info);
+        }
+    }
+
+    @media (hover: none) {
+        &:active {
+            background-color: transparent;
+            border-color: var(--color-status-info);
+        }
+    }
+}
+
+.success {
+    color: var(--color-base-white);
+    background-color: var(--color-status-success);
+    border-color: var(--color-status-success);
+
+    &:focus {
+        border-color: var(--color-theme-success);
+    }
+
+    @media (hover: hover) {
+        &:hover {
+            background-color: transparent;
+            border-color: var(--color-status-success);
+        }
+    }
+
+    @media (hover: none) {
+        &:active {
+            background-color: transparent;
+            border-color: var(--color-status-success);
+        }
+    }
+}
+
+.warning {
+    color: var(--color-base-black);
+    background-color: var(--color-status-warning);
+    border-color: var(--color-status-warning);
+
+    &:focus {
+        border-color: var(--color-theme-warning);
+    }
+
+    @media (hover: hover) {
+        &:hover {
+            background-color: transparent;
+            border-color: var(--color-status-warning);
+        }
+    }
+
+    @media (hover: none) {
+        &:active {
+            background-color: transparent;
+            border-color: var(--color-status-warning);
+        }
+    }
+}
+
+.danger {
+    color: var(--color-base-white);
+    background-color: var(--color-status-danger);
+    border-color: var(--color-status-danger);
+
+    &:focus {
+        border-color: var(--color-theme-danger);
+    }
+
+    @media (hover: hover) {
+        &:hover {
+            background-color: transparent;
+            border-color: var(--color-status-danger);
+        }
+    }
+
+    @media (hover: none) {
+        &:active {
+            background-color: transparent;
+            border-color: var(--color-status-danger);
+        }
+    }
+}
+
+.large {
+    height: 40px;
+    font-size: var(--font-size-large);
+}
+
+.medium {
+    height: 32px;
+    font-size: var(--font-size-common);
+}
+
+.small {
+    height: 24px;
+    font-size: var(--font-size-small);
+}
+
+.error {
+    font-size: var(--font-size-small);
+    color: var(--color-status-danger);
+}
+</style>
