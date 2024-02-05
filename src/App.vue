85d3619488c9d7d96b134db5cd4f4c92e34b8a21
古椿 <tsubaki@shigurezuki.jp>
2024-02-06 07:15:09 +0900

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,74 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import Button from '@/components/organisms/formParts/Button.vue';
-import Input from '@/components/organisms/formParts/Input.vue';
+import Button from '@/components/organisms/common/Button.vue';
+import Input from '@/components/organisms/controls/Input.vue';
+import Number from '@/components/organisms/controls/Number.vue';
+import Radio from '@/components/organisms/controls/Radio.vue';
+import RadioGroup from '@/components/organisms/controls/RadioGroup.vue';
+import Select from '@/components/organisms/controls/Select.vue';
 import useFormData from '@/composables/useFormData';
-import { string, object } from 'zod';
+import z from 'zod';
 
-const model = ref('もでる');
-const TEST_SCHEMA = object({
-    test: string().max(50).min(1)
-}).required();
-const { canSubmit, resetForm } = useFormData(TEST_SCHEMA, { test: 'ヤマダタロウ' });
+const initData = {
+    input: 'w',
+    number: 7,
+    radio: false,
+    radioGroup: 'dog',
+    select: 'dog'
+};
+const model = ref(initData);
+const TEST_SCHEMA = z.object({
+    input: z.string(),
+    number: z.coerce.number(),
+    radio: z.literal(true),
+    radioGroup: z.string().min(1),
+    select: z.string().min(1)
+});
+
+const items = [
+    {
+        label: '犬',
+        value: 'dog'
+    },
+    {
+        label: '猫',
+        value: 'cat'
+    },
+    {
+        label: '兎',
+        value: 'rabbit'
+    }
+];
+
+const { canSubmit, resetForm, handleSubmit } = useFormData(TEST_SCHEMA, initData);
+
+const onSubmit = handleSubmit((values) => {
+    // eslint-disable-next-line no-console
+    console.log(values);
+});
 </script>
 
 <template>
     <main>
-        <h1>テスト</h1>
-        <Button :disabled="!canSubmit">ゴーストボタン</Button>
+        <h1>テスト vee-validate</h1>
+        <Button :disabled="!canSubmit" @click="onSubmit">ゴーストボタン</Button>
         <Button @click="resetForm">reset</Button>
-        <Input name="test" :schema="TEST_SCHEMA.shape.test" />
-        <Input v-model="model" name="test2" />{{ model }}
+        <Radio :value="true" name="radio" :schema="TEST_SCHEMA.shape.radio">規約に同意</Radio>
+        <RadioGroup
+            name="radioGroup"
+            :schema="TEST_SCHEMA.shape.radioGroup"
+            :items="items"
+            label="らべる"
+        />
+        <Select name="select" :schema="TEST_SCHEMA.shape.select" :items="items" label="らべる" />
+        <Input name="input" label="らべる" :schema="TEST_SCHEMA.shape.input" />
+        <Number name="number" :schema="TEST_SCHEMA.shape.number" />
+        <h1>テスト v-model</h1>
+        <Radio :value="true" v-model="model.radio">規約に同意</Radio>
+        <RadioGroup v-model="model.radioGroup" :items="items" label="らべる" />
+        <Select v-model="model.select" :items="items" label="らべる" />
+        <Number v-model="model.number" name="number" :schema="TEST_SCHEMA.shape.number" />
+        {{ model }}
     </main>
 </template>
 
