04952c111f7b9613f3156a9896253b24f669eafa
古椿 <tsubaki@shigurezuki.jp>
2024-02-10 15:56:36 +0900

    Buttonのストーリー刷新

--- src/stories/organisms/common/Button.stories.ts
+++ src/stories/organisms/common/Button.stories.ts
@@ -25,7 +25,7 @@ const meta: Meta<typeof Button> = {
         shape: {
             options: ['normal', 'rounded', 'circle', 'square']
         },
-        onClick: { action: 'onClick' }
+        onClick: { action: 'click' }
     }
 };
 
@@ -34,26 +34,48 @@ type Story = StoryObj<typeof Button>;
 
 export const Default: Story = {};
 
-export const Info: Story = {
-    args: {
-        variant: 'info'
-    }
+export const PropsVariant: Story = {
+    storyName: 'Props/Variant',
+    render: () => ({
+        components: { Button },
+        template: `
+            <Button variant="primary">primary</Button>
+            <Button variant="secondary">secondary</Button>
+            <Button variant="info">info</Button>
+            <Button variant="success">success</Button>
+            <Button variant="warning">warning</Button>
+            <Button variant="danger">danger</Button>
+        `
+    })
 };
 
-export const Success: Story = {
-    args: {
-        variant: 'success'
-    }
+export const PropsSize: Story = {
+    storyName: 'Props/Variant',
+    render: () => ({
+        components: { Button },
+        template: `
+            <Button size="large">large</Button>
+            <Button size="medium">medium</Button>
+            <Button size="small">small</Button>
+        `
+    })
 };
 
-export const Warning: Story = {
-    args: {
-        variant: 'warning'
-    }
+export const PropsShape: Story = {
+    storyName: 'Props/Variant',
+    render: () => ({
+        components: { Button },
+        template: `
+            <Button shape="normal">normal</Button>
+            <Button shape="rounded">rounded</Button>
+            <Button shape="circle">circle</Button>
+            <Button shape="square">square</Button>
+        `
+    })
 };
 
-export const Danger: Story = {
+export const LongText: Story = {
     args: {
-        variant: 'danger'
+        default: '長いテキストのケース'
     }
 };
